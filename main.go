7a7d67f239a2f39652048f1fbac6072a95ee7648
komisan19 <komiyama6219@gmail.com>
2022-01-02 16:55:41 +0900

    refactor: count blog theme

--- main.go
+++ main.go
@@ -8,18 +8,23 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
+func writeReadMe() *os.File {
+	file, err := os.OpenFile("README.md", os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	return file
+}
+
 func main() {
 	feed, err := gofeed.NewParser().ParseURL("https://zenn.dev/komisan19/feed")
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
-	file, err := os.OpenFile("README.md", os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
 	for _, item := range feed.Items {
-		fmt.Fprintf(file, "- [%v](%v)\n", item.Title, item.Link)
+		fmt.Fprintf(writeReadMe(), "- [%v](%v)\n", item.Title, item.Link)
 	}
 }
